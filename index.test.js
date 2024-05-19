2a94d41bb62db396de1b305e30f16e3e5cc931c8
Test-subject-105 <sebastian105schapfel@gmail.com>
2024-05-19 02:45:21 +0000

    s

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { app } = require('./app');
+const { app } = require('./index');
 
 describe('/hello1', () => {
   it('returns `Hello World!`', async () => {
