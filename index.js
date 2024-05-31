63d537b20ba78931eb09a1ae97229f1d9afef921
105 | Sebastian <sebastianschapfel105@gmail.com>
2024-05-31 13:57:52 -0400

    s

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-const { app } = require('./app');
+/* eslint-disable no-undef */
+// Import essential libraries
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// const router = express.app();
+// eslint-disable-next-line no-undef
+
+app.use(express.static(__dirname + "/"));
+
+// Setup essential routes
+app.get("/", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get("/assets/*", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/105/error-codes/401.html"));
+});
+app.get("/about", function (req, res) {
+// eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/105/about-me/index.html"));
+});
+app.get("/game/play", function (req, res) {
+  // eslint-disable-next-line no-undef
+    res.sendFile(path.join(__dirname + "/game/games.html"));
+  });
+app.get("/search-result", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/search/result/index.html"));
+});
+
+
+app.get('/hello1', function (req, res) {
+  res.status(200).json({
+    message: "Hello World!"
+  });
+});
+app.get("/*", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "404.html"));
+});
+
 
 const PORT = 1050;
 app.listen(PORT, () => {
