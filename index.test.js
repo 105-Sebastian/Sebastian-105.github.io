f6224ed6ce9687e19d5f7e57cde0d1a6c855871c
105 | Sebastian <sebastianschapfel105@gmail.com>
2024-06-03 07:59:31 -0400

    changed the urls/uri

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const { app } = require('./app');
 
-describe('/hello1', () => {
+describe('/test105', () => {
   it('returns `Hello World!`', async () => {
-    const data = await request(app).get('/hello1');
+    const data = await request(app).get('/test105');
     expect(data.statusCode).toBe(200);
     expect(data.body.message).toBe('Hello World!');
   });
